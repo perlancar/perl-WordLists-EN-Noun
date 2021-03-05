044cb4208a48ecd2cf8f1288c6b351f86d964505
perlancar (on netbook-dell-xps13) <perlancar@gmail.com>
2021-03-05 08:31:04 +0700

    Release v0.001

--- lib/WordList/EN/Noun/TalkEnglish.pm
+++ lib/WordList/EN/Noun/TalkEnglish.pm
@@ -5,78 +5,33 @@ package WordList::EN::Noun::TalkEnglish;
 # DIST
 # VERSION
 
+use strict;
+use warnings;
 use WordList;
 our @ISA = qw(WordList);
 
-use Role::Tiny;
 use Role::Tiny::With;
-use Tables::Words::EN::Nouns::TalkEnglish;
-use TablesRole::Util::Basic; # for scan_prereqs
-
 with 'WordListRole::FromArray';
 
-my $t = Tables::Words::EN::Nouns::TalkEnglish->new;
-Role::Tiny->apply_roles_to_object($t, 'TablesRole::Util::Basic'); # for as_aoa
-my $ary = [map {$_->[0]} @{ $t->as_aoa }];
+sub _array {
+    require Tables::Words::EN::Nouns::TalkEnglish;
 
-sub _array { $ary }
+    my $t = Tables::Words::EN::Nouns::TalkEnglish->new;
+    my $ary = [];
+    while (my $row = $t->get_row_arrayref) { push @$ary, $row->[0] }
+    $ary;
+}
 
+our $DYNAMIC = 1;
 our $SORT = 'custom';
 
 # STATS
 
 1;
-# ABSTRACT: Common exception words (year 1) from twinkl.co.uk
+# ABSTRACT: Words that are used as nouns only, from talkenglish.com
 
 =head1 DESCRIPTION
 
-Source: L<https://twink.co.uk/>
+Source: L<https://www.talkenglish.com/vocabulary/top-1500-nouns.aspx>
 
 =cut
-
-__DATA__
-the
-a
-do
-to
-today
-of
-said
-says
-are
-were
-was
-is
-his
-has
-I
-you
-your
-they
-be
-he
-me
-she
-we
-no
-go
-so
-by
-my
-here
-there
-where
-love
-come
-some
-one
-once
-ask
-friend
-school
-put
-push
-pull
-full
-house
-our
